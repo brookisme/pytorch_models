08b60fcc93f9f0b42b4dad15b7905f896218857a
Brookie Guzder-Williams <bguzder-williams@wri.org>
2019-08-16 11:37:58 -0700

    LowLevelFeature:_check_stride, docs

--- pytorch_models/helpers.py
+++ pytorch_models/helpers.py
@@ -15,6 +15,30 @@ DEFAULT_DROPOUT_RATE=0.5
 # TOOLS
 #
 class LowLevelFeatures(object):
+    """
+
+    LowLevelFeatures provides two main functions:
+
+        1. Track current output stride of model and provide the correct
+           dilation (as property) and (stride) as method so the model can
+           have a fixed output stride
+
+        2. Checks the output-stride-state at various points in the model and
+           save, the return, features/channels from specific points in the 
+           forward pass
+
+    Args:
+        output_stride<int>: output_stride
+        low_level_output<int|list|str|False>:
+            - if truthy append low_level_features/low_level_channels for update_low_level_features
+            - preset options exclude the last block
+            - options:
+                - LowLevelFeatures.ALL: after all blocks
+                - LowLevelFeatures.HALF: at stride = output_stride//2
+                - A list containing output strides of interest and/or tag-strings
+        drop_array<bool>: 
+            - if True and len(low-level-outputs)==1 return value instead of array
+    """
     ALL='all'
     HALF='half'
 
@@ -31,12 +55,16 @@ class LowLevelFeatures(object):
 
 
     def increment(self,stride=2):
+        """ updates output_stride_state / dilation 
+        """
         self.output_stride_state=self.output_stride_state*stride
         if self.output_stride and (self.output_stride_state>=self.output_stride):
             self.dilation*=stride
 
 
-    def stride(self,stride):
+    def stride(self,stride=2):
+        """ returns the correct (dilation dependent) stride
+        """
         if self.dilation==1:
             return stride
         else:
@@ -44,18 +72,29 @@ class LowLevelFeatures(object):
 
 
     def update_low_level_features(self,x,ch,tag=None):
+        """
+
+        Checks current output_stride_state and tag to see if it is equal to
+        or in low_level_output.  If True, appends x and ch to low_level_features and
+        low_level_channels respectively.
+
+        Args:
+            x<nn.Module>: feature to possibly add to low_level_features
+            ch<int>: corresponding channel for low_level_feature
+            tag<str|None>: a string indicating which low_level_feature is being tested
+        """
         if self.low_level_output:
             if isinstance(self.low_level_output,int):
-                is_low_level_feature=self.low_level_output==self.output_stride_state
+                is_low_level_feature=self._check_stride(self.low_level_output)
             elif isinstance(self.low_level_output,str):
                 if self.low_level_output==LowLevelFeatures.ALL:
                     is_low_level_feature=True
                 elif self.low_level_output==LowLevelFeatures.HALF:
-                    is_low_level_feature=self.half_output_stride==self.output_stride_state
+                    is_low_level_feature=self._check_stride(self.half_output_stride)
                 else:
                     is_low_level_feature=self.low_level_output==tag
             else:
-                state_is_in=self.output_stride_state in self.low_level_output
+                state_is_in=any([self._check_stride(s) for s in self.low_level_output])
                 if tag:
                     tag_is_in=tag in self.low_level_output
                     is_low_level_feature=state_is_in or tag_is_in
@@ -67,6 +106,9 @@ class LowLevelFeatures(object):
 
 
     def out(self):
+        """ returns low_level_features/channels 
+        * if drop_array=True and if len(low_level_features)==1, return values not arr
+        """
         if self.drop_array and (len(self.low_level_channels)==1):
             return self.low_level_features[0], self.low_level_channels[0]
         else:
@@ -74,6 +116,8 @@ class LowLevelFeatures(object):
 
 
     def reset(self):
+        """ reset instance
+        """
         try:
             del(self.low_level_features)
             del(self.low_level_channels)
@@ -83,6 +127,15 @@ class LowLevelFeatures(object):
         self.dilation=1
         self.low_level_features=[]
         self.low_level_channels=[]
+        self._existing_strides=[]
+
+
+    def _check_stride(self,stride):
+        if isinstance(stride,int):
+            if stride not in self._existing_strides:
+                if stride==self.output_stride_state:
+                    self._existing_strides.append(stride)
+                    return True
 
 
 #
