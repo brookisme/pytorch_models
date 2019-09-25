6edce573aa5bc1a8248a400806ab99f7d8f18391
Brookie Guzder-Williams <bguzder-williams@wri.org>
2019-09-24 23:00:20 -0400

    dlv3p: classifer block. fixed up_chs and reducer_out_chs

--- pytorch_models/deeplab/model.py
+++ pytorch_models/deeplab/model.py
@@ -26,13 +26,13 @@ class DeeplabV3plus(nn.Module):
         backbone_config<dict>:
             - kwarg-dict for backbone
             - in_ch set explicitly above
-        self.low_level_out_chs<int|None>:
+        low_level_out_chs<int|None>:
             - out_chs for low_level_features in backbone
             - if None attempts to get value from backbone-instance
         backbone_out_ch<int>:
             - out_ch from backbone
             - if None attempts to get value from backbone-instance        
-        reduced_low_level_out_ch: 
+        reducer_out_chs<int|float|list|None>
             - the number of channels to reduce low-level-features to
             - before concatenation
         aspp_out_ch=256,
@@ -49,6 +49,13 @@ class DeeplabV3plus(nn.Module):
             - depth for refinement_conv block
         refinement_conv_config<dict>:
             - kwarg-dict for Conv block
+        classifier_position<str>: 
+            - determines is the classifer comes before or after the final upsample
+            - values are 'before' or 'after'
+        classifier_depth<int>:
+            - depth for classifier block (can be overwritten by classifier config)
+        classifier_config<dict>:
+            - kwarg dict for Conv Block
         out_activation<str|func|False|None>: 
             - activation method or method name
             - False: No output activation
@@ -58,8 +65,10 @@ class DeeplabV3plus(nn.Module):
     XCEPTION='xception'
     RESNET='resnet'
     UPMODE='bilinear'
-    NB_STEPS=2
     NOT_IMPLEMENTED="Currently only supports 'xception' and 'resnet' backbone"
+    BEFORE_UP='before'
+    AFTER_UP='after'
+
 
     """TODO
     - StrideManger LLO now has steps. need to fix dlv3+ res/xcept to use
@@ -68,7 +77,7 @@ class DeeplabV3plus(nn.Module):
     def __init__(self,
             in_ch,
             out_ch,
-            out_chs=None,
+            up_chs=256,
             backbone=XCEPTION,
             backbone_config={
                 'output_stride': 16,
@@ -77,8 +86,7 @@ class DeeplabV3plus(nn.Module):
             backbone_low_level_out_chs=None,
             backbone_scale_factors=None,
             backbone_out_ch=None,
-            reduced_low_level_out_chs=None,
-            reducer_ch_reduction=0.5,
+            reducer_out_chs=48,
             aspp=True,
             aspp_out_ch=256,
             aspp_config={},
@@ -86,6 +94,9 @@ class DeeplabV3plus(nn.Module):
             upsample_mode=UPMODE,
             refinement_conv_depth=2,
             refinement_conv_config={},
+            classifier_position=BEFORE_UP,
+            classifier_depth=1,
+            classifier_config={},            
             out_activation=False,
             out_activation_config={}):
         super(DeeplabV3plus,self).__init__()
@@ -106,17 +117,18 @@ class DeeplabV3plus(nn.Module):
         else:
             self.aspp=False
             refinement_in_ch=self.bb_out_ch
-        reducer_chs=self._reducer_chs(reduced_low_level_out_chs,reducer_ch_reduction)
-        self.reducers=self._reducers(reducer_chs)
+        reducer_out_chs=self._reducer_out_chs(reducer_out_chs)
+        self.reducers=self._reducers(reducer_out_chs)
+        up_chs=self._up_chs(up_chs)
         self.refinement_convs=self._refinement_convs(
             refinement_in_ch,
-            self.low_level_out_chs,
-            reducer_chs,
-            out_ch,
-            out_chs,
+            reducer_out_chs,
+            up_chs,
             dropout,
             refinement_conv_depth,
             refinement_conv_config)
+        self.classifier_position=classifier_position
+        self.classifier=self._classifier(up_chs[-1],out_ch,classifier_depth,classifier_config)
         self.act=output_activation(out_activation,out_activation_config)
 
             
@@ -129,14 +141,18 @@ class DeeplabV3plus(nn.Module):
                 self.low_level_out_chs,
                 self.reducers,
                 self.scale_factors[:-1],
-                self.refinement_convs):          
+                self.refinement_convs):
             x=self._up(x,scale_factor=sf)
             lx=red(lx)
             x=torch.cat([x,lx],dim=1)
             if ref:
                 x=ref(x)
+        if self.classifier_position==DeeplabV3plus.BEFORE_UP:
+            x=self.classifier(x)
         if self.scale_factors[-1]>1:
             x=self._up(x,scale_factor=self.scale_factors[-1])
+        if self.classifier_position==DeeplabV3plus.AFTER_UP:
+            x=self.classifier(x)
         if self.act:
             x=self.act(x)
         return x
@@ -167,18 +183,22 @@ class DeeplabV3plus(nn.Module):
             raise NotImplementedError(NOT_IMPLEMENTED)
 
 
-    def _reducer_chs(self,reducer_chs,reduction):
-        if reducer_chs:
-            reducer_chs=reducer_chs[::-1]
+    def _reducer_out_chs(self,reducer_out_chs):
+        if isinstance(reducer_out_chs,int):
+            reducer_out_chs=[reducer_out_chs]*len(self.low_level_out_chs)
+        elif isinstance(reducer_out_chs,float):
+            reducer_out_chs=[int(reducer_out_chs*ch) for ch in self.low_level_out_chs]
+        elif reducer_out_chs:
+            reducer_out_chs=reducer_out_chs[::-1]
         else:
-            reducer_chs=[int(reduction*ch) for ch in self.low_level_out_chs]
-        return reducer_chs
+            reducer_out_chs=[False]*len(self.low_level_out_chs)
+        return reducer_out_chs
 
 
-    def _reducers(self,reducer_chs):
+    def _reducers(self,reducer_out_chs):
         reducers=[]
-        for in_ch,out_ch in zip(self.low_level_out_chs,reducer_chs):
-            if in_ch==out_ch:
+        for in_ch,out_ch in zip(self.low_level_out_chs,reducer_out_chs):
+            if (in_ch==out_ch) or (not out_ch):
                 reducers.append(nn.Identity())
             else:
                 reducers.append(nn.Conv2d(
@@ -188,12 +208,18 @@ class DeeplabV3plus(nn.Module):
         return nn.ModuleList(reducers)
 
 
+    def _up_chs(self,up_chs):
+        if isinstance(up_chs,int):
+            up_chs=[up_chs]*len(self.low_level_out_chs)
+        elif not up_chs:
+            up_chs=self.low_level_out_chs
+        return up_chs
+
+
     def _refinement_convs(self,
             encoder_out_ch,
-            ll_outs,
-            red_outs,
-            out_ch,
-            out_chs,
+            reducer_out_chs,
+            up_chs,
             dropout,
             depth,
             config):
@@ -202,17 +228,20 @@ class DeeplabV3plus(nn.Module):
             config['dropout']=dropout
         if config.get('depth') is None:
             config['depth']=depth
-        if not out_chs:
-            out_chs=[out_ch]*len(ll_outs)
-        feat_ins=[encoder_out_ch]+out_chs[:-1]
-        rl_outs=[ r or l for r,l in zip(red_outs,ll_outs)]
+        feat_ins=[encoder_out_ch]+up_chs
+        rl_outs=[ r or l for r,l in zip(reducer_out_chs,self.low_level_out_chs)]
         in_chs=[ f+rl for f,rl in zip(feat_ins,rl_outs)]
-        for in_ch,out_ch in zip(in_chs,out_chs):
+        for in_ch,out_ch in zip(in_chs,up_chs):
             if out_ch:
                 refiners.append(Conv(in_ch=in_ch,out_ch=out_ch,**config))
             else:
                 refiners.append(False)
         return nn.ModuleList(refiners)
+
+
+    def _classifier(self,in_ch,out_ch,depth,config):
+        config['depth']=config.get('depth',depth)
+        return Conv(in_ch=in_ch,out_ch=out_ch,**config)
 
 
     def _up(self,x,scale_factor=4):
